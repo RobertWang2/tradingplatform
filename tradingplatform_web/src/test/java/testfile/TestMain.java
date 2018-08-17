b576bb8e94a2132f5ded739eb022ce91bb3072d3
Robert_Wang <3255893782@qq.com>
2018-08-17 10:13:07 +0800

    Tradingplatform

---
+++ tradingplatform_web/src/test/java/testfile/TestMain.java
@@ -0,0 +1,34 @@
+package testfile;
+
+import com.xyz.biz.UserService;
+import com.xyz.dao.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @program: tradingplatform
+ * @description:
+ * @author: Robert_Wang
+ * @create: 2018-08-17 08:18
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestMain {
+
+    @Resource(name = "userService")
+    private UserService userService;
+
+    @Test
+    public void test(){
+        System.out.println("before");
+        User user = userService.queryByUsername("Robert_Wang");
+        System.out.println("Username :" + user.getAge());
+        System.out.println("end");
+
+    }
+}
+
